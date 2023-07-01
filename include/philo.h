e46921dd62bca6159e75ec23f039cb3b8ff76bfb
AhlyelAmine <aa.ahlyel.amine@gmail.com>
2023-07-01 01:50:33 +0100

    building

--- include/philo.h
+++ include/philo.h
@@ -2,5 +2,29 @@
 # define PHILO_H
 
 #include "../lib/libft/include/libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/time.h>
+typedef struct s_philo
+{
+	int	philos;
+	int	tm_eat;
+	int	tm_sleep;
+	int	tm_die;
+	int	eat_counter;
+	int	current_philo;
+}   t_philo;
 
+typedef struct s_philo_single_data
+{
+	pthread_mutex_t	mtx;
+	pthread_mutex_t	left;
+	pthread_mutex_t	right;
+	pthread_t		thread;
+	t_philo			lp;
+}   t_philo_single_data;
 #endif
