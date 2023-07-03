ca4ea05734ab7737da325dbd81c77d5721769e28
AhlyelAmine <aa.ahlyel.amine@gmail.com>
2023-07-03 17:14:23 +0100

    building...

--- include/philo.h
+++ include/philo.h
@@ -16,15 +16,30 @@ typedef struct s_philo
 	int	tm_sleep;
 	int	tm_die;
 	int	eat_counter;
-	int	current_philo;
 }   t_philo;
 
 typedef struct s_philo_single_data
 {
+	int				philo_id;
+	bool			dead;
+	bool			eat;
+	bool			sleep;
+	long int		time_to_die;
 	pthread_mutex_t	mtx;
 	pthread_mutex_t	left;
 	pthread_mutex_t	right;
+	pthread_mutex_t	lock;
 	pthread_t		thread;
 	t_philo			lp;
 }   t_philo_single_data;
+
+typedef enum s_msg
+{
+	FORK	= 0b1,
+	EAT		= 0b10,
+	THINK	= 0b100,
+	SLEEP	= 0b1000,
+	DEAD	= 0b10000
+}	t_msg;
+
 #endif
