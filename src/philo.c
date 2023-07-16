115c3a211a97dbae0b1052eeebfb5d84129a3b2f
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-07-16 03:31:06 +0100

    reboot old catch(thread for each thread) by improving it

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 23:57:02 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/07/15 06:01:01 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/07/16 03:04:04 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int main(int ac, char **av)
 		if (get_data(&data, ac, av))
 			return (1);
 	}
-	if (ac == 5)
+	if (ac == 5 || ac == 6)
 		take_forks(init_data(data), data);
-	else if (ac == 6)
-		;
+	// else if (ac == 6)
+	// 	;
 	else
 		ft_putendl_fd("invalid arguments", 2);
 	return (0);
